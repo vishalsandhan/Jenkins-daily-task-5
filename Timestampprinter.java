6319db85a117389b1f43090b578c258431b0cf5c
VishalSandhan <vishalsandhan1@gmail.com>
2025-02-11 13:37:28 +0530

    added initial files

---
+++ Timestampprinter.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+
+public class Timestampprinter {
+    public static void main(String[] args) {
+        System.out.println("Current Timestamp: " + LocalDateTime.now());
+    }
+}
